2e315da96d99c3f8028e61a86cf8f57c8a3af7b3
Todd Yamakawa <todd.yamakawa@arm.com>
2016-07-15 03:28:23 +0000

    gpio: Adding Raspberry Pi 3 GPIO pins.

---
+++ setup/gpio_rpi3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo " 01 02 | 3.3V 5V "
+echo " 03 04 |   02 5V "
+echo " 05 06 |   03 GND"
+echo " 07 08 |   04 14 "
+echo " 09 10 |  GND 15 "
+
+echo " 11 12 |   17 18 "
+echo " 13 14 |   27 GND"
+echo " 15 16 |   22 23 "
+echo " 17 18 | 3.3V 24 "
+echo " 19 20 |   10 GND"
+
+echo " 21 22 |   09 25 "
+echo " 23 24 |   11 08 "
+echo " 25 26 |  GND 07 "
+echo " 27 28 |         "
+echo " 29 30 |   05 GND"
+
+echo " 31 32 |   06 12 "
+echo " 33 34 |   13 GND"
+echo " 35 36 |   19 16 "
+echo " 37 38 |   26 20 "
+echo " 39 40 |  GND 21 "
+
+
