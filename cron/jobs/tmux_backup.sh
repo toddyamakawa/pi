f3df801c80a103a3ba9f74f684cd82013f31eeab
Todd Yamakawa <todd.yamakawa@arm.com>
2016-07-18 23:23:38 +0000

    cron: Updated tmux_backup.sh script.

--- cron/jobs/tmux_backup.sh
+++ cron/jobs/tmux_backup.sh
@@ -1,52 +1,6 @@
 #!/bin/bash
 here=$(dirname $(readlink -f $0))
 timestamp=$(date +%b%d_%H%M%S)
-backup=$here/tmux_backup/$timestamp
-sourceme=$backup/sourceme.bash
-tmux=/usr/bin/tmux
-
-mkdir -p $backup
-echo "here=$backup" > $sourceme
-
-sessions=$($tmux list-sessions -F '#{session_name}')
-for session in $sessions; do
-	script=$backup/$session.bash
-
-	windows=$($tmux list-windows -t $session -F '#{window_id} #{window_name} #{window_layout}')
-
-	# For Each Window
-	window_index=0
-	while read -r window; do
-		window_id=$(echo $window | awk '{print $1}')
-		window_name=$(echo $window | awk '{print $2}')
-		window_layout=$(echo $window | awk '{print $3}')
-
-		panes=$($tmux list-panes -t $session:$window_id -F '#{pane_current_path}')
-
-		# For Each Pane
-		pane_index=0
-		while read -r pane; do
-
-			if [[ $window_index == 0 ]]; then
-				if [[ $pane_index == 0 ]]; then
-					echo "$tmux new-session -s $session -n $window_name -c $pane -d \\; source-file \$here/$session.bash" >> $sourceme
-				else
-					echo "split-window -c $pane" >> $script
-				fi
-			else
-				if [[ $pane_index == 0 ]]; then
-					echo "new-window -n $window_name -c $pane" >> $script
-				else
-					echo "split-window -c $pane" >> $script
-				fi
-			fi
-
-			pane_index=$(($pane_index+1))
-		done <<< "$panes"
-		echo "select-layout $window_layout" >> $script
-
-		window_index=$(($window_index+1))
-	done <<< "$windows"
-
-done
-
+log=$here/../tmux_backup/$timestamp.tmux.log
+format="'#{session_name} #{window_index} #{window_name} #{pane_current_path} #{window_layout}'"
+eval tmux list-panes -a -F $format > $log
