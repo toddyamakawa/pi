5d7e57e34275079b014acf675b0b03e63cc41b66
Todd Yamakawa <todd.yamakawa@arm.com>
2016-07-19 02:41:56 +0000

    cron: Updating speedtest with units.

--- cron/jobs/speedtest.sh
+++ cron/jobs/speedtest.sh
@@ -3,5 +3,5 @@ here=$(dirname $(readlink -f $0))
 log=$here/../logs/speedtest.log
 #download=http://speedtest.wdc01.softlayer.com/downloads/test10.zip
 download=http://speedtest.wdc01.softlayer.com/downloads/test100.zip
-speed=$(wget -O /dev/null $download 2>&1 | awk '/saved/{print substr($3,2)}')
-echo $(date +'%D %T') $(bc <<< 8*$speed) >> $log
+speed=$(wget -O /dev/null $download 2>&1 | awk '/saved/{print $1,$2,$3,$4}')
+echo $speed >> $log
